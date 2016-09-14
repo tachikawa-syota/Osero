45dd1e94a98eec7780500328ca80e79bf9117873
tachikawa-syota <s.tachikawa1105@gmail.com>
2016-09-14 11:17:48 +0900

    クライアント側実装 一人でオセロをプレイできるところまで

---
+++ client/Assets/Scene/WebSocketClient.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using WebSocketSharp;
+
+public class WebSocketClient : MonoBehaviour 
+{
+	private WebSocket ws;
+
+	void OnGUI() {
+		if (GUILayout.Button("Connect")) {
+			this.ws = new WebSocket("ws://127.0.0.1:8080");
+			this.ws.OnMessage += (object sender, MessageEventArgs e) => {
+				print (e.Data);
+			};
+			this.ws.Connect ();
+		}
+
+		if (GUILayout.Button("Send")) {
+			this.ws.Send (System.DateTime.Now.ToString ());
+		}
+
+		if (GUILayout.Button("Close")) {
+			this.ws.Close ();
+		}
+	}
+}
