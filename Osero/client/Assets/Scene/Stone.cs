732cdf126e122ea577167aa0003caece77e945fa
tachikawa-syota <s.tachikawa1105@gmail.com>
2016-09-16 00:18:52 +0900

    Add files via upload

---
+++ Osero/client/Assets/Scene/Stone.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+// Jsonを使えるようにする
+using Newtonsoft.Json;
+
+// 石を置くクラス
+public class Stone : MonoBehaviour 
+{
+	// 更新
+	void  Update ()
+	{
+        // 右クリックが入力されたら
+		if (Input.GetButtonDown("Fire1")) 
+		{
+            // ワールド変換
+			Vector3 screenPoint= Input.mousePosition;      
+			screenPoint.z = 10;
+			Vector3 v= Camera.main.ScreenToWorldPoint(screenPoint);
+			float key_x= Mathf.Floor(v.x);
+			float key_y= Mathf.Floor(v.z);
+
+			// パケットにキー座標情報をセット
+			Packet data = new Packet();
+			data.keyPos.x = key_x;
+			data.keyPos.y = key_y;
+
+            // ウェブソケットのインスタンス作成
+			WebSocketClient ws = new WebSocketClient ();
+            // データをサーバーに転送
+			ws.SendData ();
+
+            // GameControllerの"putPiece"関数を呼び出す
+			GameObject.FindWithTag("GameController").SendMessage("putPiece", new Vector2(key_x, key_y));
+		}
+	}
+}
---
+++ Osero/client/Assets/Scene/WebSocketClient.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using WebSocketSharp;
+// Jsonを使えるようにする
+using Newtonsoft.Json;
+
+/**
+* @brief WebSoketClient
+*/
+public class WebSocketClient : MonoBehaviour 
+{
+	// ウェブソケット
+	private WebSocket ws;
+	// パケット
+	private Packet packet;
+
+    // 初期化
+	void Start()
+	{
+		// 接続
+		ws = new WebSocket("ws://127.0.0.1:25678");
+		ws.Log.Level = LogLevel.Trace;
+
+		ws.OnOpen += (sender, e) =>
+		{
+			Debug.Log("opened.");
+		};
+
+		ws.OnMessage += (sender, e) =>
+		{
+			Debug.Log("onmessage.");
+		};
+
+		ws.OnError += (sender, e) =>
+		{
+			Debug.Log("error:" + e.Message);
+		};
+
+		ws.OnClose += (sender, e) =>
+		{
+			Debug.Log("closed." + e.Code.ToString() + e.Reason.ToString());
+		};
+
+		// コネクト
+		ws.Connect ();
+	}
+
+	// サーバー側にデータを転送する
+	public void SendData()
+	{
+		// パケットを取得
+		Packet data = new Packet();
+
+		// インスタンスを取得
+		data.payload = new Payload();
+		data.payload.name = "tachikawa";
+		data.method = "ping";
+
+		// シリアライズする
+		string method = JsonUtility.ToJson(data);
+
+        // デバッグログ表示
+		Debug.Log (method);
+		// サーバーに送る
+		ws.Send (method);
+	}
+
+
+//		mothod: "keypos",
+//		payload: {
+//			x: 10,
+//			y: 10,
+//		}
+
+    // UIの描画
+	void OnGUI() {
+		if (GUILayout.Button("Connect"))
+        {
+			ws = new WebSocket("ws://localhost:25678/");
+			ws.OnMessage += (object sender, MessageEventArgs e) => {
+			//	print (e.Data);
+			};
+			ws.Connect ();
+		}
+
+        //if(Input.GetButtonDown("Fire2")){
+        //    // パケットを取得
+        //    Packet data = new Packet();
+        //    // インスタンスを取得
+        //    data.payload = new Payload();
+        //    data.payload.name = "tachikawa";
+        //    data.method = "ping";
+
+        //    // シリアライズする
+        //    string method = JsonUtility.ToJson(data);
+
+        //    Debug.Log (method);
+        //    // サーバーに送る
+        //    ws.Send (method);
+        //}
+
+	}
+}
